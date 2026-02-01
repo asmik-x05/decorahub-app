2f48bbc7c8c9d8c218bbc2969afc3f80e0c5be48
Asmik Lamichhane <asmiklmc.x05@gmail.com>
2026-02-01 15:51:59 +0545

    add product fetch with filter

--- src/api/products.js
+++ src/api/products.js
@@ -2,16 +2,29 @@ import axios from "axios";
 import config from "@/config/config";
 
 export const getHotPick = async () => {
-
-  const response = await axios.get(`${config.apiUrl}/api/product?limit=4&offset=1`);
+  const response = await axios.get(
+    `${config.apiUrl}/api/product?limit=4&offset=1`,
+  );
 
   return response.data;
 };
 export const getRecommended = async () => {
-
-  const response = await axios.get(`${config.apiUrl}/api/product?limit=4&offset=5`);
+  const response = await axios.get(
+    `${config.apiUrl}/api/product?limit=4&offset=5`,
+  );
 
   return response.data;
 };
+export const getProducts = async ({ searchParams }) => {
+  const sort = (await searchParams)?.sort ?? "";
+  const min = (await searchParams)?.min ?? "";
+  const max = (await searchParams)?.max ?? "";
+  const category = (await searchParams)?.category ?? "";
+  const brand = (await searchParams)?.brand ?? "";
+  const name = (await searchParams)?.name ?? "";
+  const response = await axios.get(
+    `${config.apiUrl}/api/product?sort=${sort}&min=${min}&max=${max}&category=${category}&brand=${brand}&name=${name}`,
+  );
 
-
+  return response.data;
+};
